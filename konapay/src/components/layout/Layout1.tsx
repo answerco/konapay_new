2027fbb09d8f154f63f21faa17a115ec44b81f14
Answer-Hector <81742859+Answer-Hector@users.noreply.github.com>
2022-04-14 14:27:21 +0900

    layout1_grid

--- konapay/src/components/layout/Layout1.tsx
+++ konapay/src/components/layout/Layout1.tsx
@@ -1,15 +1,26 @@
 import './Layout1.css';
+import React from 'react';
+import { IonGrid, IonRow, IonCol, IonContent } from '@ionic/react';
+
+const meta = document.createElement('meta');
+meta.name = "viewport";
+meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover";
+document.getElementsByTagName('head')[0].appendChild(meta);
 
 interface ContainerProps {
   name: string;
 }
 
-const Layout1: React.FC<ContainerProps> = ({ name }) => {
+const Layout1: React.FC<ContainerProps> = () => {
   return (
-    <div className="container">
-      <strong>{name}</strong>
-        
-    </div>
+    <div className="grid-init grid">
+    <div className="box-init box">A</div>
+    <div className="box-init box">B</div> 
+    <div className="box-init box">C</div> 
+    <div className="box-init box">D</div> 
+    <div className="box-init box">E</div> 
+    <div className="box-init box">F</div> 
+  </div>
   );
 };
 
