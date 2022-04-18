8c20df58a42743089c2c9c5fa65d852d6bbbe24b
Answer-Hector <81742859+Answer-Hector@users.noreply.github.com>
2022-04-18 18:01:04 +0900

    mm

--- konapay/src/components/layout/Main/Main.tsx
+++ konapay/src/components/layout/Main/Main.tsx
@@ -14,7 +14,14 @@ import {
     IonPage ,
     IonButtons,
     IonButton,
-    IonItem 
+    IonItem, 
+    IonImg,
+    IonNote,
+    IonCardContent,
+    IonCardTitle,
+    IonCardHeader,
+    IonCard,
+    IonCardSubtitle
 } from '@ionic/react';
 
 import React from 'react';
@@ -41,7 +48,16 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+import { 
+    homeOutline,
+    settingsOutline,
+    personCircleOutline,
+    logoUsd,
+    giftOutline
+} 
+from 'ionicons/icons';
 
+import './main.css'
 
   
   
@@ -53,18 +69,27 @@ import '@ionic/react/css/display.css';
   meta.content = "width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover";
   document.getElementsByTagName('head')[0].appendChild(meta);
   
+
+  
+  
+
+  
   
   const Main: React.FC = () => {
+ 
+
+      
     return (
     <IonApp>
         <IonMenu content-id="main-content">
         <IonHeader>
-            <IonToolbar color="light" border-width="200%">
+            <IonToolbar color="light">
                 <IonTitle>KONA PAY</IonTitle>
             </IonToolbar>
         </IonHeader>
 
-        <IonContent>
+
+        <IonContent content-id="main-content">
             <IonList>
                 <IonListHeader>
                     Navigate
@@ -82,29 +107,115 @@ import '@ionic/react/css/display.css';
         </IonContent>
     </IonMenu>
 
-    <IonPage className="ion-page" id="main-content">
+
+    <IonPage className="ion-page" >
         <IonHeader>
-        <IonToolbar>
-                <IonButtons slot="start">
+            <IonToolbar>
+
+                    <IonButtons slot="start" id="main-content">
+                        <IonMenuToggle>
+                            <IonButton>
+                                <IonIcon name="menu-sharp" slot='start'></IonIcon>
+                            </IonButton>
+                        </IonMenuToggle>
+                    </IonButtons>
+
+                    <IonButtons slot="end" id="myinfo" >
+                        <IonMenuToggle>
+                            <IonButton>
+                                <IonIcon src={personCircleOutline} ></IonIcon>
+                            </IonButton>
+                        </IonMenuToggle>
+                    </IonButtons>
+
+                    <IonButtons slot="end" id="setting" >
+                        <IonMenuToggle>
+                            <IonButton>
+                                <IonIcon src={settingsOutline} ></IonIcon>
+                            </IonButton>
+                        </IonMenuToggle>
+                    </IonButtons>
+
+                    <IonButtons slot="end" id="home" >
+                        <IonMenuToggle>
+                            <IonButton>
+                                <IonIcon src={homeOutline} ></IonIcon>
+                            </IonButton>
+                        </IonMenuToggle>
+                    </IonButtons>
+
+            </IonToolbar>
+        </IonHeader>
+
+        <IonContent className="background"></IonContent>
+        <IonLabel className="bannerText" >KONA PAY</IonLabel>
+        <IonLabel className="bannerText2" >언제 어디서든 간편 한 결제</IonLabel>
+
+
+        <IonCard className="walletCard">
+            <IonCardHeader>
+                <IonLabel className="cardHeader">
+                    카드선택
+                </IonLabel>
+            </IonCardHeader>
+            <IonImg src={MainCard} className="mainCard"/>
+            
+            <IonCardSubtitle>
+                <IonLabel className="cardSub">
+                    ● ○ ○ ○ 
+                </IonLabel>
+            </IonCardSubtitle>
+        </IonCard>
+        
+        <IonButton color="medium" className="pwdBtn" ><a style={{color:'white'}}>비밀번호</a></IonButton>
+        <IonLabel className="scanTopay">
+            스캔으로 결제하세요
+        </IonLabel>
+
+            <IonToolbar className="mainFooter" style={{backgroundColor:'rgb(230, 230, 230)', height:'15%' ,paddingTop:'3%'}}>
+
+
+                <IonButtons slot="start" id="home" style={{marginLeft:'10%'}}>
                     <IonMenuToggle>
                         <IonButton>
-                            <IonIcon name="menu-outline" slot='start'></IonIcon>
+                            <a>
+                                <IonIcon src={homeOutline} ></IonIcon>
+                                <div><IonLabel>홈</IonLabel></div>
+                            </a>
                         </IonButton>
                     </IonMenuToggle>
                 </IonButtons>
 
-                <IonTitle>
 
-                </IonTitle>
+                <IonButtons slot="start" style={{marginLeft:'20%'}}>
+                    <IonMenuToggle>
+                        <IonButton>
+                            <a>
+                                <IonIcon src={logoUsd}></IonIcon>
+                                <div style={{height:'100%'}}><IonLabel>결제요청</IonLabel></div>
+                            </a>
+                        </IonButton>
+                    </IonMenuToggle>
+                </IonButtons>
 
-        </IonToolbar>
-        </IonHeader>
+                <IonButtons slot="start" style={{marginLeft:'20%'}}>
+                    <IonMenuToggle>
+                        <IonButton>
+                            <a>
+                                <IonIcon src={giftOutline}></IonIcon>
+                                <div><IonLabel>혜택</IonLabel></div>
+                            </a>
+                        </IonButton>
+                    </IonMenuToggle>
+                </IonButtons>
+            </IonToolbar>
 
-        <IonContent className="ion-padding">
-        asdasdasdsd
-        </IonContent>
+
+            
+
 
     </IonPage>
+
     </IonApp>
     );
   };
