3ff0728c919c83d0dbd8158977adedf45846c4dc
Answer-kino <kino@ianswer.co.kr>
2022-04-19 15:02:11 +0900

    refect: camera testPage init

--- konapay/src/components/camera/Layout.tsx
+++ konapay/src/components/camera/Layout.tsx
@@ -1,23 +1,23 @@
-import "./Layout.css";
+// import "./Layout.css";
 import React from "react";
-import { Camera, CameraOptions } from "@awesome-cordova-plugins/camera";
+
+import { IonButton, IonContent, IonHeader, IonPage, IonToolbar } from "@ionic/react";
 
 const meta = document.createElement("meta");
 meta.name = "viewport";
 meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover";
 document.getElementsByTagName("head")[0].appendChild(meta);
 
-interface ContainerProps {
-  name: string;
-}
-
-const captureImage = () => {};
-
 const Layout: React.FC = () => {
+  const openScanner = async () => {};
   return (
-    <div className="grid-init grid">
-      <p>camera Test Page</p>
-    </div>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>Tab 1</IonToolbar>
+        <IonButton onClick={openScanner}>Scan barcode</IonButton>
+      </IonHeader>
+      <IonContent></IonContent>
+    </IonPage>
   );
 };
 
