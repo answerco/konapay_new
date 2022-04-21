2575112adcaf9742bc3c6dcba2df345e5453d1bf
Answer-kino <kino@ianswer.co.kr>
2022-04-21 10:39:28 +0900

    feat: BoardRead page layout ver.2

--- konapay/src/components/layout/Board/BoardRead.tsx
+++ konapay/src/components/layout/Board/BoardRead.tsx
@@ -1,33 +1,8 @@
-import {
-  IonApp,
-  IonButton,
-  IonButtons,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCol,
-  IonContent,
-  IonDatetime,
-  IonFooter,
-  IonGrid,
-  IonHeader,
-  IonIcon,
-  IonInput,
-  IonItem,
-  IonItemDivider,
-  IonList,
-  IonPage,
-  IonRow,
-  IonSelect,
-  IonSelectOption,
-  IonText,
-  IonTextarea,
-  IonTitle,
-} from "@ionic/react";
-import { chevronBack, heart, star } from "ionicons/icons";
+import { IonApp, IonCard, IonCardContent, IonCardHeader, IonCol, IonContent, IonFooter, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonPage, IonRow, IonText, IonTitle } from "@ionic/react";
+import { chevronBack, heart } from "ionicons/icons";
 import React, { useState } from "react";
 import { useHistory } from "react-router";
-
+import Menu from "./menu";
 interface BoardReadInterface {
   boderId: number | string;
   category: string;
@@ -53,14 +28,23 @@ const BoardRead: React.FC = () => {
   const history = useHistory();
   const category = "test category";
   const title = "test title";
-  const content = `조금 긴 글을 쓰고 싶은데 할말은 없고 퍼블리싱은 어렵고 집갈 생각하니 벌써부터 토가나오네 어휴 ,, 이거 어떻게 해야하나...`;
+  const content = `조금 긴 글을 쓰고 싶은데 할말은 없고 퍼블리싱은 어렵고 집갈 생각하니 벌써부터 토가나오네 어휴 ,, 이거 어떻게 해야하나...조금 긴 글을 쓰고 싶은데 할말은 없고 퍼블리싱은 어렵고 집갈 생각하니 벌써부터 토가나오네 어휴 ,, 이거 어떻게 해야하나...조금 긴 글을 쓰고 싶은데 할말은 없고 퍼블리싱은 어렵고 집갈 생각하니 벌써부터 토가나오네 어휴 ,, 이거 어떻게 해야하나...조금 긴 글을 쓰고 싶은데 할말은 없고 퍼블리싱은 어렵고 집갈 생각하니 벌써부터 토가나오네 어휴 ,, 이거 어떻게 해야하나...조금 긴 글을 쓰고 싶은데 할말은 없고 퍼블리싱은 어렵고 집갈 생각하니 벌써부터 토가나오네 어휴 ,, 이거 어떻게 해야하나...조금 긴 글을 쓰고 싶은데 할말은 없고 퍼블리싱은 어렵고 집갈 생각하니 벌써부터 토가나오네 어휴 ,, 이거 어떻게 해야하나...`;
   const date = "2022-04-20T10:47:20Z";
   const count = 2;
   const [likeCount, setLikeCount] = useState(0);
   return (
     <IonApp>
+      <Menu></Menu>
       <IonPage>
         <IonContent>
+          <IonHeader>
+            <IonItem>
+              <IonItem button onClick={() => history.goBack()}>
+                <IonIcon icon={chevronBack}></IonIcon>
+              </IonItem>
+              <IonTitle>게시판</IonTitle>
+            </IonItem>
+          </IonHeader>
           <IonCard>
             <IonCardHeader>
               <IonItem>{category}</IonItem>
@@ -81,7 +65,8 @@ const BoardRead: React.FC = () => {
                 <IonInput value={title} readonly></IonInput>
               </IonItem>
               <IonItem>
-                <IonTextarea autoGrow value={content} readonly></IonTextarea>
+                {/* <IonTextarea rows={10} value={content} readonly></IonTextarea> */}
+                <IonText>{content}</IonText>
               </IonItem>
             </IonCardContent>
             <IonFooter>
@@ -106,95 +91,8 @@ const BoardRead: React.FC = () => {
                   </IonRow>
                   <IonItem>
                     <IonCol>
-                      <IonTextarea autoGrow value={content} readonly></IonTextarea>
-                    </IonCol>
-                  </IonItem>
-                </IonGrid>
-              </IonItem>
-              <IonItem>
-                <IonIcon icon={heart} color="danger" onClick={() => setLikeCount(likeCount + 1)}></IonIcon>
-                <IonText>{likeCount}</IonText>
-              </IonItem>
-            </IonCard>
-            <IonCard class="뎃글 카드 시작점">
-              <IonItem>
-                <IonGrid>
-                  <IonRow>
-                    <div style={{ width: "60px", height: "60px", backgroundColor: "black", borderRadius: "50%" }} />
-                    <IonCol>
-                      <IonRow>작성자</IonRow>
-                      <IonRow>{date}</IonRow>
-                    </IonCol>
-                  </IonRow>
-                  <IonItem>
-                    <IonCol>
-                      <IonTextarea autoGrow value={content} readonly></IonTextarea>
-                    </IonCol>
-                  </IonItem>
-                </IonGrid>
-              </IonItem>
-              <IonItem>
-                <IonIcon icon={heart} color="danger" onClick={() => setLikeCount(likeCount + 1)}></IonIcon>
-                <IonText>{likeCount}</IonText>
-              </IonItem>
-            </IonCard>
-            <IonCard class="뎃글 카드 시작점">
-              <IonItem>
-                <IonGrid>
-                  <IonRow>
-                    <div style={{ width: "60px", height: "60px", backgroundColor: "black", borderRadius: "50%" }} />
-                    <IonCol>
-                      <IonRow>작성자</IonRow>
-                      <IonRow>{date}</IonRow>
-                    </IonCol>
-                  </IonRow>
-                  <IonItem>
-                    <IonCol>
-                      <IonTextarea autoGrow value={content} readonly></IonTextarea>
-                    </IonCol>
-                  </IonItem>
-                </IonGrid>
-              </IonItem>
-              <IonItem>
-                <IonIcon icon={heart} color="danger" onClick={() => setLikeCount(likeCount + 1)}></IonIcon>
-                <IonText>{likeCount}</IonText>
-              </IonItem>
-            </IonCard>
-            <IonCard class="뎃글 카드 시작점">
-              <IonItem>
-                <IonGrid>
-                  <IonRow>
-                    <div style={{ width: "60px", height: "60px", backgroundColor: "black", borderRadius: "50%" }} />
-                    <IonCol>
-                      <IonRow>작성자</IonRow>
-                      <IonRow>{date}</IonRow>
-                    </IonCol>
-                  </IonRow>
-                  <IonItem>
-                    <IonCol>
-                      <IonTextarea autoGrow value={content} readonly></IonTextarea>
-                    </IonCol>
-                  </IonItem>
-                </IonGrid>
-              </IonItem>
-              <IonItem>
-                <IonIcon icon={heart} color="danger" onClick={() => setLikeCount(likeCount + 1)}></IonIcon>
-                <IonText>{likeCount}</IonText>
-              </IonItem>
-            </IonCard>
-            <IonCard class="뎃글 카드 시작점">
-              <IonItem>
-                <IonGrid>
-                  <IonRow>
-                    <div style={{ width: "60px", height: "60px", backgroundColor: "black", borderRadius: "50%" }} />
-                    <IonCol>
-                      <IonRow>작성자</IonRow>
-                      <IonRow>{date}</IonRow>
-                    </IonCol>
-                  </IonRow>
-                  <IonItem>
-                    <IonCol>
-                      <IonTextarea autoGrow value={content} readonly></IonTextarea>
+                      {/* <IonTextarea autoGrow value={content} readonly></IonTextarea> */}
+                      <IonText>value={content}</IonText>
                     </IonCol>
                   </IonItem>
                 </IonGrid>
@@ -205,7 +103,6 @@ const BoardRead: React.FC = () => {
               </IonItem>
             </IonCard>
           </IonCard>
-          <IonList></IonList>
         </IonContent>
       </IonPage>
     </IonApp>
